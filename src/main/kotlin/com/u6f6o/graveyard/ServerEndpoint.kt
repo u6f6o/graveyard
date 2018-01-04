b0d3f931896659a43f12795cba8e87a8e022c1d1
Ulf Gitschthaler <ulf@gitschthaler.de>
2018-01-05 00:24:39 +0100

    going to sleep now

--- src/main/kotlin/com/u6f6o/graveyard/ServerEndpoint.kt
+++ src/main/kotlin/com/u6f6o/graveyard/ServerEndpoint.kt
@@ -1,14 +1,37 @@
 package com.u6f6o.graveyard
 
-import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.buffer.ByteBufAllocator
+import io.netty.channel.ChannelFuture
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.ChannelOption
+import io.netty.channel.socket.SocketChannel
+import java.net.InetSocketAddress
 
-class ServerEndpoint(port: Int) {
-    val port: Int = port
 
+class ServerEndpoint(
+        private val port: Int,
+        private val workerThreads: Int,
+        private var cf: ChannelFuture?) {
+
+    @Throws(Exception::class)
     fun run() {
-        val bossGroup = NioEventLoopGroup()
-        val workerGroup = NioEventLoopGroup();
+        val transport = ServerTransportType.initialize(workerThreads)
+        val bootstrap = ServerBootstrap()
 
-        ServerTransportType.initialize(10)
+        bootstrap.group(transport.bossgroup, transport.workerGroup)
+                .channel(transport.channelClazz)
+                .childOption(ChannelOption.ALLOCATOR, ByteBufAllocator.DEFAULT)
+                .option(ChannelOption.ALLOCATOR, ByteBufAllocator.DEFAULT)
+                .handler(object : ChannelInitializer<SocketChannel>() {
+                    @Throws(Exception::class)
+                    public override fun initChannel(ch: SocketChannel) {
+                        val pipeline = ch.pipeline()
+                        pipeline.addLast("decoder", MQTTDecoder())
+                        pipeline.addLast("encoder", MQTTEncoder())
+                        pipeline.addLast("handler", handler)
+                    }
+                })
+        this.cf = bootstrap.bind(InetSocketAddress(port)).sync()
     }
 }
